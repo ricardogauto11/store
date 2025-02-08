0df6210d3da29b58f1fff90842a0cb5ae50ac666
Ricardo Gauto <ricardo.gauto@shakeagain.net>
2025-02-08 15:02:06 -0300

    Se agregaron status code

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -12,12 +23,35 @@ export class ProductsController {
     @Query('limit') limit: number,
     @Query('offset') offset: number,
     @Query('brand') brand: string,
-  ): string {
-    return `Products Filter: limit ${limit}, offset ${offset}, brand ${brand}`;
+  ): object {
+    return {
+      message: `Products Filter: limit ${limit}, offset ${offset}, brand ${brand}`,
+    };
   }
 
   @Get(':id')
+  @HttpCode(HttpStatus.ACCEPTED)
   getProduct(@Param('id') id: number): string {
     return `Product ${id}`;
+  }
+
+  @Post()
+  create(@Body() payload: object) {
+    return { message: 'Acción de crear', payload };
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: object) {
+    return {
+      message: `Acción de actualizar ${id}`,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return {
+      message: `Acción de eliminar ${id}`,
+    };
   }
 }
